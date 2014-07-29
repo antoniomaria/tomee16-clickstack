0795ec9234ea22ea2516e2c34f98eba40f79de2e
Antonio Sanchez” <antonio@feedbackdialog.com>
2014-07-29 09:17:32 +0300

    clickstack template

---
+++ README.md
@@ -0,0 +1,304 @@
+# Tomcat 8 ClickStack
+
+To use: 
+
+    bees app:deploy -t tomcat8 -a APP_ID WAR_FILE path/to/my/app.war
+
+
+Tomcat 8 ClickStack for CloudBees PaaS.
+
+
+# Build 
+
+    $  gradlew clean installClickstack distClickstack
+
+After successful build
+
+* an expanded `tomcat8-clickstack`is created under `build/install` and can be used with your local [genapp](http://genapp-docs.cloudbees.com/).
+* `tomcat8-clickstack-1.2.3.zip` is created under `build/distributions` and can be uploaded to the CloudBees platform location by the CloudBees team.
+
+# Local development
+
+Note: You should be familiar with developing ClickStacks using the genapp system first. \[[see docs](http://genapp-docs.cloudbees.com/quickstart.html)\]
+
+* Build the plugin project using make to prepare for use in local app deploys
+* In plugins\_home, add a symlink to the `tomcat8-clickstack/build/install/tomcat8-clickstack` dir named 'tomcat8'
+
+   ```
+   $ ln -s tomcat8-clickstack/build/install/tomcat8-clickstack $GENAPP_PLUGINS_HOME/tomcat8
+   ```
+
+* In your metadata.json, you can now reference the stack using the name 'tomcat8'
+
+   ```
+   { "app": {  "plugins": ["tomcat8"] } }
+   ```
+
+Once the plugin is published to a public URL, you can update an app to use it with the CloudBees SDK:
+
+   ```
+$ bees app:deploy -a APP_ID -t tomcat8 -RPLUGIN.SRC.tomcat8=URL_TO_YOUR_PLUGIN_ZIP PATH_TO_WARFILE
+```
+
+# Key concepts
+
+
+* `build.gradle` : the gradle build to create the clickstack
+* `com.cloudbees.clickstack.tomcat.Setup`: the setup code that instantiate the clickstack
+
+
+## Clickstack layout
+
+<code><pre>
+└── tomcat8-clickstack
+    ├── deps <== DEPS TO ADD TO THE DEPLOYED APP
+    │   ├── control-lib <== DEPS FOR CONTROL SCRIPTS
+    │   │   └── … .jar
+    │   ├── javaagent-lib <== DEPS FOR JAVA AGENTS
+    │   │   └── ... .jar
+    │   ├── tomcat-lib <== DEPS TO UNCONDITIONALLY ADD TO TOMCAT LIB
+    │   │   └── ... .jar
+    │   ├── tomcat-lib-mail <== DEPS TO ADD IF A MAIL SESSION IS CONFIGURED (SENDGRID)
+    │   │   └── ... .jar
+    │   ├── tomcat-lib-memcache <== DEPS TO ADD IF MEMCACHE BASED SESSION REPLICATION IS CONFIGURED
+    │   │   └── ... .jar
+    │   ├── tomcat-lib-mysql <== DEPS TO ADD IF a MYSQL DATABASE IS BOUND TO THE APP
+    │   │   └── ... .jar
+    │   └── tomcat-lib-postgresql <== DEPS TO ADD IF a POSTGRESQL DATABASE IS BOUND TO THE APP
+    │       └── ... .jar
+    │
+    ├── dist  <== FILES THAT WILL BE COPIED DIRECTLY UNDER APP_DIR
+    │   ├── .genapp
+    │   │   └── control
+    │   │       ├── functions
+    │   │       │   └── functions
+    │   │       ├── jmx_invoker
+    │   │       ├── print_environment
+    │   │       ├── send_sigquit
+    │   │       ├── start
+    │   │       └── stats-appstat
+    │   └── catalina-base
+    │       └── conf
+    │           ├── context.xml
+    │           ├── logging.properties
+    │           ├── server.xml
+    │           ├── tomcat-metrics.xml
+    │           └── web.xml
+    │ 
+    ├── lib <== JARS USED BY THE SETUP SCRIPT
+    │   ├── ...
+    │   ├── tomcat8-clickstack-1.0.0-SNAPSHOT.jar
+    │   └── ...
+    │
+    ├── setup
+    ├── setup.bat
+    └── tomcat-8.0.0-RC10.zip <== TOMCAT PACKAGE TO DEPLOY
+</pre></code>
+
+### ClickStack Detailed layout
+
+<code><pre>
+└── tomcat8-clickstack
+    ├── deps
+    │   ├── control-lib
+    │   │   └── cloudbees-jmx-invoker-1.0.2-jar-with-dependencies.jar
+    │   ├── javaagent-lib
+    │   │   ├── cloudbees-clickstack-javaagent-1.2.0.jar
+    │   │   ├── jmxtrans-agent-1.0.6.jar
+    │   │   └── jsr305-2.0.1.jar
+    │   ├── tomcat-lib
+    │   │   └── cloudbees-web-container-extras-1.0.1.jar
+    │   ├── tomcat-lib-mail
+    │   │   ├── activation-1.1.jar
+    │   │   └── mail-1.4.7.jar
+    │   ├── tomcat-lib-memcache
+    │   │   ├── annotations-1.3.9.jar
+    │   │   ├── asm-3.2.jar
+    │   │   ├── jsr305-1.3.9.jar
+    │   │   ├── kryo-1.04.jar
+    │   │   ├── kryo-serializers-0.10.jar
+    │   │   ├── memcached-session-manager-1.6.4.jar
+    │   │   ├── memcached-session-manager-tc7-1.6.4.jar
+    │   │   ├── minlog-1.2.jar
+    │   │   ├── msm-kryo-serializer-1.6.4.jar
+    │   │   ├── reflectasm-1.01.jar
+    │   │   └── spymemcached-2.8.12.jar
+    │   ├── tomcat-lib-mysql
+    │   │   └── mysql-connector-java-5.1.25.jar
+    │   └── tomcat-lib-postgresql
+    │       └── postgresql-9.1-901-1.jdbc4.jar
+    ├── dist
+    │   ├── .genapp
+    │   │   └── control
+    │   │       ├── functions
+    │   │       │   └── functions
+    │   │       ├── jmx_invoker
+    │   │       ├── print_environment
+    │   │       ├── send_sigquit
+    │   │       ├── start
+    │   │       └── stats-appstat
+    │   └── catalina-base
+    │       └── conf
+    │           ├── context.xml
+    │           ├── logging.properties
+    │           ├── server.xml
+    │           ├── tomcat-metrics.xml
+    │           └── web.xml
+    ├── lib
+    │   ├── Saxon-HE-9.4.jar
+    │   ├── clickstack-framework-1.0.0.jar
+    │   ├── dom4j-1.6.1.jar
+    │   ├── guava-14.0.1.jar
+    │   ├── hamcrest-core-1.3.jar
+    │   ├── jackson-annotations-2.1.2.jar
+    │   ├── jackson-core-2.1.3.jar
+    │   ├── jackson-databind-2.1.3.jar
+    │   ├── jdom-1.1.jar
+    │   ├── jsr305-2.0.1.jar
+    │   ├── slf4j-api-1.7.5.jar
+    │   ├── slf4j-simple-1.7.5.jar
+    │   ├── tomcat8-clickstack-1.0.0-SNAPSHOT.jar
+    │   ├── xalan-2.7.0.jar
+    │   ├── xercesImpl-2.8.0.jar
+    │   ├── xml-matchers-1.0-RC1.jar
+    │   ├── xml-resolver-1.2.jar
+    │   ├── xmlunit-1.3.jar
+    │   └── xom-1.2.5.jar
+    ├── setup
+    ├── setup.bat
+    └── tomcat-8.0.0-RC10.zip    </pre></code>
+    
+# Deployed Application Layout
+
+<code><pre>
+├── .genapp
+│   ├── control
+│   │   ├── config
+│   │   ├── env
+│   │   ├── functions
+│   │   │   └── functions
+│   │   ├── java-opts-10-core
+│   │   ├── java-opts-20-javaagent
+│   │   ├── java-opts-20-tomcat-opts
+│   │   ├── java-opts-60-jmxtrans-agent
+│   │   ├── jmx_invoker
+│   │   ├── print_environment
+│   │   ├── send_sigquit
+│   │   ├── start
+│   │   └── stats-appstat
+│   ├── lib
+│   │   ├── cloudbees-jmx-invoker-1.0.2-jar-with-dependencies.jar
+│   │   └── cloudbees-jmx-invoker-jar-with-dependencies.jar -> .../.genapp/lib/cloudbees-jmx-invoker-1.0.2-jar-with-dependencies.jar
+│   ├── log
+│   ├── metadata.json
+│   ├── ports
+│   │   └── 8604
+│   └── setup_status
+│       ├── ok
+│       └── plugin_tomcat8_clickstack_0
+├── apache-tomcat-8.0.0-RC10
+│   ├── LICENSE
+│   ├── NOTICE
+│   ├── RELEASE-NOTES
+│   ├── RUNNING.txt
+│   ├── bin
+│   │   ├── bootstrap.jar
+│   │   ├── catalina-tasks.xml
+│   │   ├── catalina.bat
+│   │   ├── catalina.sh
+│   │   ├── commons-daemon-native.tar.gz
+│   │   ├── commons-daemon.jar
+│   │   ├── configtest.bat
+│   │   ├── configtest.sh
+│   │   ├── cpappend.bat
+│   │   ├── daemon.sh
+│   │   ├── digest.bat
+│   │   ├── digest.sh
+│   │   ├── setclasspath.bat
+│   │   ├── setclasspath.sh
+│   │   ├── shutdown.bat
+│   │   ├── shutdown.sh
+│   │   ├── startup.bat
+│   │   ├── startup.sh
+│   │   ├── tomcat-juli.jar
+│   │   ├── tomcat-native.tar.gz
+│   │   ├── tool-wrapper.bat
+│   │   ├── tool-wrapper.sh
+│   │   ├── version.bat
+│   │   └── version.sh
+│   ├── conf
+│   │   ├── catalina.policy
+│   │   ├── catalina.properties
+│   │   ├── context.xml
+│   │   ├── logging.properties
+│   │   ├── server.xml
+│   │   ├── tomcat-users.xml
+│   │   └── web.xml
+│   ├── lib
+│   │   ├── annotations-api.jar
+│   │   ├── catalina-ant.jar
+│   │   ├── catalina-ha.jar
+│   │   ├── catalina-storeconfig.jar
+│   │   ├── catalina-tribes.jar
+│   │   ├── catalina.jar
+│   │   ├── ecj-4.2.2.jar
+│   │   ├── el-api.jar
+│   │   ├── jasper-el.jar
+│   │   ├── jasper.jar
+│   │   ├── jsp-api.jar
+│   │   ├── servlet-api.jar
+│   │   ├── tomcat-api.jar
+│   │   ├── tomcat-coyote.jar
+│   │   ├── tomcat-dbcp.jar
+│   │   ├── tomcat-i18n-es.jar
+│   │   ├── tomcat-i18n-fr.jar
+│   │   ├── tomcat-i18n-ja.jar
+│   │   ├── tomcat-jdbc.jar
+│   │   ├── tomcat-jni.jar
+│   │   ├── tomcat-spdy.jar
+│   │   ├── tomcat-util.jar
+│   │   ├── tomcat-websocket.jar
+│   │   └── websocket-api.jar
+│   ├── logs
+│   ├── temp
+│   │   └── safeToDelete.tmp
+│   ├── webapps
+│   │   ├── ROOT
+│   │   │   └── ...
+│   │   ├── docs
+│   │   │   └── ...
+│   │   ├── examples
+│   │   │   └── ...
+│   │   ├── host-manager
+│   │   │   └── ...
+│   │   └── manager
+│   │       └── ...
+│   └── work
+├── app-extra-files
+├── catalina-base
+│   ├── conf
+│   │   ├── context.xml
+│   │   ├── logging.properties
+│   │   ├── server.xml
+│   │   ├── tomcat-metrics.xml
+│   │   └── web.xml
+│   ├── lib
+│   │   ├── activation-1.1.jar
+│   │   ├── cloudbees-web-container-extras-1.0.1.jar
+│   │   ├── mail-1.4.7.jar
+│   │   └── mysql-connector-java-5.1.25.jar
+│   ├── logs
+│   ├── webapps
+│   │   └── ROOT
+│   │       ├── WEB-INF
+│   │       │   ├── classes
+│   │       │   │   └── *.class
+│   │       │   ├── lib
+│   │       │   │   └── *.jar
+│   │       │   └── web.xml
+│   │       └── *.jsp ...
+│   └── work
+├── javaagent-lib
+│   ├── cloudbees-clickstack-javaagent-1.2.0.jar
+│   └── jmxtrans-agent-1.0.6.jar
+└── tmp</pre></code>
---
+++ src/main/java/com/cloudbees/clickstack/tomcat/Setup.java
@@ -0,0 +1,423 @@
+/*
+ * Copyright 2010-2013, CloudBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.cloudbees.clickstack.tomcat;
+
+import com.cloudbees.clickstack.domain.environment.Environment;
+import com.cloudbees.clickstack.domain.metadata.*;
+import com.cloudbees.clickstack.plugin.java.JavaPlugin;
+import com.cloudbees.clickstack.plugin.java.JavaPluginResult;
+import com.cloudbees.clickstack.util.CommandLineUtils;
+import com.cloudbees.clickstack.util.Files2;
+import com.cloudbees.clickstack.util.Manifests;
+import com.google.common.base.Charsets;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
+import com.google.common.collect.Iterables;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Properties;
+
+public class Setup {
+
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+    @Nonnull
+    final Path appDir;
+    @Nonnull
+    final Path genappDir;
+    @Nonnull
+    final Path controlDir;
+    @Nonnull
+    final Path clickstackDir;
+    @Nonnull
+    final Path javaHome;
+    @Nonnull
+    final Path catalinaBase;
+    @Nonnull
+    final Path warFile;
+    @Nonnull
+    final Path logDir;
+    @Nonnull
+    final Path tmpDir;
+    @Nonnull
+    final Path agentLibDir;
+    @Nonnull
+    final Path appExtraFilesDir;
+    @Nonnull
+    final Metadata metadata;
+    @Nonnull
+    final Environment env;
+    /**
+     * initialised by {@link #installCatalinaHome()}
+     */
+    @Nullable
+    Path catalinaHome;
+
+
+    public Setup(@Nonnull Environment env, @Nonnull Metadata metadata, @Nonnull Path javaHome) throws IOException {
+        logger.info("Setup: {}, {}", env, metadata);
+
+        this.env = env;
+        this.appDir = env.appDir;
+
+        this.genappDir = env.genappDir;
+
+        this.controlDir = env.controlDir;
+        this.logDir = Files.createDirectories(genappDir.resolve("log"));
+        Files2.chmodAddReadWrite(logDir);
+
+        this.catalinaBase = Files.createDirectories(appDir.resolve("catalina-base"));
+
+        this.agentLibDir = Files.createDirectories(appDir.resolve("javaagent-lib"));
+
+        this.tmpDir = Files.createDirectories(appDir.resolve("tmp"));
+        Files2.chmodAddReadWrite(tmpDir);
+
+        this.clickstackDir = env.clickstackDir;
+        Preconditions.checkState(Files.exists(clickstackDir) && Files.isDirectory(clickstackDir));
+
+        this.warFile = env.packageDir.resolve("app.war");
+        Preconditions.checkState(Files.exists(warFile), "File not found %s", warFile);
+        Preconditions.checkState(!Files.isDirectory(warFile), "Expected to be a file and not a directory %s", warFile);
+
+        this.appExtraFilesDir = Files.createDirectories(appDir.resolve("app-extra-files"));
+        Files2.chmodAddReadWrite(appExtraFilesDir);
+
+        this.metadata = metadata;
+
+        this.javaHome = Preconditions.checkNotNull(javaHome, "javaHome");
+        Preconditions.checkArgument(Files.exists(javaHome), "JavaHome does not exist %s", javaHome);
+
+        logger.debug("warFile: {}", warFile.toAbsolutePath());
+        logger.debug("catalinaBase: {}", catalinaBase.toAbsolutePath());
+        logger.debug("agentLibDir: {}", agentLibDir.toAbsolutePath());
+        logger.debug("appExtraFilesDir: {}", appExtraFilesDir.toAbsolutePath());
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            Logger initialisationLogger = LoggerFactory.getLogger(Setup.class);
+
+            initialisationLogger.info("Setup clickstack {} - {}, current dir {}",
+                    Manifests.getAttribute(Setup.class, "Implementation-Artifact"),
+                    Manifests.getAttribute(Setup.class, "Implementation-Date"),
+                    FileSystems.getDefault().getPath(".").toAbsolutePath());
+            Environment env = CommandLineUtils.argumentsToEnvironment(args);
+            Path metadataPath = env.genappDir.resolve("metadata.json");
+            Metadata metadata = Metadata.Builder.fromFile(metadataPath);
+
+            JavaPlugin javaPlugin = new JavaPlugin();
+            JavaPluginResult javaPluginResult = javaPlugin.setup(metadata, env);
+
+            Setup setup = new Setup(env, metadata, javaPluginResult.getJavaHome());
+            setup.setup();
+        } catch (Exception e) {
+            String hostname;
+            try {
+                hostname = InetAddress.getLocalHost().getHostAddress();
+            } catch (Exception e2) {
+                hostname = "#hostname#";
+            }
+            throw new Exception("Exception deploying on " + hostname, e);
+        }
+    }
+
+    public void setup() throws Exception {
+        installCatalinaHome();
+        installSkeleton();
+        Path catalinaBase = installCatalinaBase();
+        installEnvClickstackProvided();
+        installCloudBeesJavaAgent();
+        installJmxTransAgent();
+        writeJavaOpts();
+        writeConfig();
+        installControlScripts();
+        installTomcatJavaOpts();
+
+        SetupTomcatConfigurationFiles setupTomcatConfigurationFiles = new SetupTomcatConfigurationFiles(metadata);
+        setupTomcatConfigurationFiles.buildTomcatConfigurationFiles(catalinaBase);
+        logger.info("Clickstack successfully installed");
+    }
+
+    public void installSkeleton() throws IOException {
+        logger.debug("installSkeleton() {}", appDir);
+
+        Files2.copyDirectoryContent(clickstackDir.resolve("dist"), appDir);
+    }
+
+    public void installTomcatJavaOpts() throws IOException {
+        Path optsFile = controlDir.resolve("java-opts-20-tomcat-opts");
+        logger.debug("installTomcatJavaOpts() {}", optsFile);
+
+        String opts = "" +
+                "-Djava.io.tmpdir=\"" + tmpDir + "\" " +
+                "-Dcatalina.home=\"" + catalinaHome + "\" " +
+                "-Dcatalina.base=\"" + catalinaBase + "\" " +
+                "-Dapp_extra_files=\"" + appExtraFilesDir + "\" " +
+                "-Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager " +
+                "-Djava.util.logging.config.file=\"" + catalinaBase + "/conf/logging.properties\"";
+
+        Files.write(optsFile, Collections.singleton(opts), Charsets.UTF_8);
+    }
+
+    public void installCatalinaHome() throws Exception {
+
+        Path tomcatPackagePath = Files2.findArtifact(clickstackDir, "tomcat", "zip");
+        Files2.unzip(tomcatPackagePath, appDir);
+        catalinaHome = Files2.findUniqueDirectoryBeginningWith(appDir, "apache-tomcat");
+        logger.debug("installCatalinaHome() {}", catalinaHome);
+
+        Files2.chmodReadOnly(catalinaHome);
+
+        // some frameworks like Grails try to write in ${catalina.home}/logs
+        Path logsDir = Files.createDirectories(catalinaHome.resolve("logs"));
+        Files2.chmodAddReadWrite(logsDir);
+
+        Path workDir = Files.createDirectories(catalinaHome.resolve("work"));
+        Files2.chmodAddReadWrite(workDir);
+
+        Path tempDir = Files.createDirectories(catalinaHome.resolve("temp"));
+        Files2.chmodAddReadWrite(tempDir);
+
+    }
+
+    public Path installCatalinaBase() throws IOException {
+        logger.debug("installCatalinaBase() {}", catalinaBase);
+
+        Files.createDirectories(catalinaBase.resolve("work"));
+
+        Files.createDirectories(catalinaBase.resolve("logs"));
+
+        String contextPath = metadata.getRuntimeParameter("webapp", "contextPath", null);
+        if (contextPath == null) {
+            contextPath = "ROOT";
+        } else {
+            if (contextPath.startsWith("/")) {
+                contextPath = contextPath.substring(1);
+            }
+            if(contextPath.isEmpty()) {
+                contextPath = "ROOT";
+            }
+            logger.info("Deploy application under custom contextPath '/{}'", contextPath);
+        }
+
+        // WEB APP
+        Path rootWebAppDir = Files.createDirectories(catalinaBase.resolve("webapps").resolve(contextPath));
+        Files2.unzip(warFile, rootWebAppDir);
+
+        // CONFIGURATION FILES
+        Path webAppBundledContextXmlFile = rootWebAppDir.resolve("META-INF/context.xml");
+        Path catalinaBaseContextXml = this.catalinaBase.resolve("conf/context.xml");
+        if (Files.exists(webAppBundledContextXmlFile) && !Files.isDirectory(webAppBundledContextXmlFile)) {
+            logger.info("Copy application provided context.xml");
+            Files.move(catalinaBaseContextXml, catalinaBase.resolve("conf/context-initial.xml"));
+            Files.copy(webAppBundledContextXmlFile, catalinaBaseContextXml);
+        }
+
+        Path webAppBundledServerXmlFile = rootWebAppDir.resolve("META-INF/server.xml");
+        Path catalinaBaseServerXml = this.catalinaBase.resolve("conf/server.xml");
+        if (Files.exists(webAppBundledServerXmlFile) && !Files.isDirectory(webAppBundledServerXmlFile)) {
+            logger.info("Copy application provided server.xml");
+            Files.move(catalinaBaseServerXml, catalinaBase.resolve("conf/server-initial.xml"));
+            Files.copy(webAppBundledServerXmlFile, catalinaBaseServerXml);
+        }
+
+        Path webAppBundledExtraFiles = rootWebAppDir.resolve("META-INF/extra-files");
+        if (Files.exists(webAppBundledExtraFiles) && Files.isDirectory(webAppBundledExtraFiles)) {
+            logger.info("Copy application provided extra files");
+            Files2.copyDirectoryContent(webAppBundledExtraFiles, this.appExtraFilesDir);
+        }
+
+        Path webAppBundledExtraLibs = rootWebAppDir.resolve("META-INF/lib");
+        if (Files.exists(webAppBundledExtraLibs) && Files.isDirectory(webAppBundledExtraLibs)) {
+            logger.info("Copy application provided extra libs");
+            Files2.copyDirectoryContent(webAppBundledExtraLibs, this.catalinaBase.resolve("lib"));
+        }
+
+        // LIBRARIES
+
+        Path targetLibDir = Files.createDirectories(catalinaBase.resolve("lib"));
+        Files2.copyDirectoryContent(clickstackDir.resolve("deps/tomcat-lib"), targetLibDir);
+
+        // JDBC Drivers
+        Collection<Database> mysqlDatabases = Collections2.filter(metadata.getResources(Database.class), new Predicate<Database>() {
+            @Override
+            public boolean apply(@Nullable Database database) {
+                return Database.DRIVER_MYSQL.equals(database.getDriver());
+            }
+        });
+        if (!mysqlDatabases.isEmpty()) {
+            logger.debug("Add mysql jars");
+            Files2.copyDirectoryContent(clickstackDir.resolve("deps/tomcat-lib-mysql"), targetLibDir);
+        }
+
+        Collection<Database> postgresqlDatabases = Collections2.filter(metadata.getResources(Database.class), new Predicate<Database>() {
+            @Override
+            public boolean apply(@Nullable Database database) {
+                return Database.DRIVER_POSTGRES.equals(database.getDriver());
+            }
+        });
+        if (!postgresqlDatabases.isEmpty()) {
+            Files2.copyDirectoryContent(clickstackDir.resolve("deps/tomcat-lib-postgresql"), targetLibDir);
+        }
+
+        // Mail
+        if (!metadata.getResources(Email.class).isEmpty()) {
+            logger.debug("Add mail jars");
+            Files2.copyDirectoryContent(clickstackDir.resolve("deps/tomcat-lib-mail"), targetLibDir);
+        }
+
+        // Memcache
+        if (!metadata.getResources(SessionStore.class).isEmpty()) {
+            logger.debug("Add memcache jars");
+            Files2.copyDirectoryContent(clickstackDir.resolve("deps/tomcat-lib-memcache"), targetLibDir);
+        }
+
+
+        Files2.chmodAddReadWrite(catalinaBase);
+
+        return catalinaBase;
+    }
+
+    public void installJmxTransAgent() throws IOException {
+        logger.debug("installJmxTransAgent() {}", agentLibDir);
+
+        Path jmxtransAgentJarFile = Files2.copyArtifactToDirectory(clickstackDir.resolve("deps/javaagent-lib"), "jmxtrans-agent", agentLibDir);
+        Path jmxtransAgentConfigurationFile = catalinaBase.resolve("conf/tomcat-metrics.xml");
+        Preconditions.checkState(Files.exists(jmxtransAgentConfigurationFile), "File %s does not exist", jmxtransAgentConfigurationFile);
+        Path jmxtransAgentDataFile = logDir.resolve("tomcat-metrics.data");
+
+        Path agentOptsFile = controlDir.resolve("java-opts-60-jmxtrans-agent");
+
+        String agentOptsFileData =
+                "-javaagent:" + jmxtransAgentJarFile.toString() + "=" + jmxtransAgentConfigurationFile.toString() +
+                        " -Dtomcat_metrics_data_file=" + jmxtransAgentDataFile.toString();
+
+        Files.write(agentOptsFile, Collections.singleton(agentOptsFileData), Charsets.UTF_8);
+    }
+
+    public void installCloudBeesJavaAgent() throws IOException {
+        logger.debug("installCloudBeesJavaAgent() {}", agentLibDir);
+
+        Path cloudbeesJavaAgentJarFile = Files2.copyArtifactToDirectory(clickstackDir.resolve("deps/javaagent-lib"), "cloudbees-clickstack-javaagent", this.agentLibDir);
+        Path agentOptsFile = controlDir.resolve("java-opts-20-javaagent");
+
+        Path envFile = controlDir.resolve("env");
+        if (!Files.exists(envFile)) {
+            throw new IllegalStateException("'env' file not found at " + envFile);
+        }
+
+        Path envClickstackProvidedFile = controlDir.resolve("env-clickstack-provided");
+        if (!Files.exists(envClickstackProvidedFile)) {
+            throw new IllegalStateException("'env-clickstack-provided' file not found at " + envClickstackProvidedFile);
+        }
+
+        // 'env' file declared after 'env-clickstack-provided' because customer provided values must override clickstack-provided values
+        String agentOptsFileData = "-javaagent:" +
+                cloudbeesJavaAgentJarFile +
+                "=" + envClickstackProvidedFile + "," + envFile;
+
+
+        Files.write(agentOptsFile, Collections.singleton(agentOptsFileData), Charsets.UTF_8);
+    }
+
+    public void writeJavaOpts() throws IOException {
+        Path javaOptsFile = controlDir.resolve("java-opts-10-core");
+        logger.debug("writeJavaOpts() {}", javaOptsFile);
+
+        String javaOpts = metadata.getRuntimeParameter("java", "opts", "");
+        Files.write(javaOptsFile, Collections.singleton(javaOpts), Charsets.UTF_8);
+    }
+
+    public void writeConfig() throws IOException {
+
+        Path configFile = controlDir.resolve("config");
+        logger.debug("writeConfig() {}", configFile);
+
+        PrintWriter writer = new PrintWriter(Files.newOutputStream(configFile));
+
+        writer.println("app_dir=\"" + appDir + "\"");
+        writer.println("app_extra_files=\"" + appExtraFilesDir + "\"");
+        writer.println("app_tmp=\"" + appDir.resolve("tmp") + "\"");
+        writer.println("log_dir=\"" + logDir + "\"");
+        writer.println("catalina_home=\"" + catalinaHome + "\"");
+        writer.println("catalina_base=\"" + catalinaBase + "\"");
+
+        writer.println("port=" + env.appPort);
+
+        writer.println("JAVA_HOME=\"" + javaHome + "\"");
+        writer.println("java=\"" + javaHome.resolve("bin/java") + "\"");
+        writer.println("genapp_dir=\"" + genappDir + "\"");
+
+        writer.println("catalina_opts=\"-Dport.http=" + env.appPort + "\"");
+
+        String classpath = "" +
+                catalinaHome.resolve("bin/bootstrap.jar") + ":" +
+                catalinaHome.resolve("bin/tomcat-juli.jar") + ":" +
+                catalinaHome.resolve("lib");
+        writer.println("java_classpath=\"" + classpath + "\"");
+
+        writer.close();
+    }
+
+    public void installEnvClickstackProvided() throws IOException {
+        Properties envClickstackProvided = new Properties();
+
+        Collection<Syslog> syslogConfigs = metadata.getResources(Syslog.class);
+        if (syslogConfigs.size() == 1) {
+            Syslog syslog = Iterables.getOnlyElement(syslogConfigs);
+            envClickstackProvided.put("SYSLOG_HOST", syslog.getHostname());
+            envClickstackProvided.put("SYSLOG_PORT", String.valueOf(syslog.getPort()));
+            envClickstackProvided.put("SYSLOG_FACILITY", syslog.getFacility());
+            envClickstackProvided.put("SYSLOG_APP_NAME", syslog.getAppName(env));
+            envClickstackProvided.put("SYSLOG_APP_HOSTNAME", syslog.getAppHostname(env));
+
+
+        } else {
+            // TODO: inject env variables suffixed by the "sanitized" resource name (e.g. )
+            logger.warn("More or less ({}) than 1 syslog configuration found, don't inject configuration in : {}", syslogConfigs.size(), syslogConfigs);
+
+        }
+
+        OutputStream out = Files.newOutputStream(controlDir.resolve("env-clickstack-provided"));
+        envClickstackProvided.store(out, "Generated by tomcat-clickstack");
+    }
+
+    public void installControlScripts() throws IOException {
+        logger.debug("installControlScripts() {}", controlDir);
+
+        Files2.chmodAddReadExecute(controlDir);
+
+        Path genappLibDir = genappDir.resolve("lib");
+        Files.createDirectories(genappLibDir);
+
+        Path jmxInvokerPath = Files2.copyArtifactToDirectory(clickstackDir.resolve("deps/control-lib"), "cloudbees-jmx-invoker", genappLibDir);
+        // create symlink without version to simplify jmx_invoker script
+        Files.createSymbolicLink(genappLibDir.resolve("cloudbees-jmx-invoker-jar-with-dependencies.jar"), jmxInvokerPath);
+    }
+}
